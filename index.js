cb61115670d0a26d4cceb2b0e06afb44d2cac752
Alexander Sapegin <sapegin04@mail.ru>
2024-04-16 18:58:10 +0500

    make drink format

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-(function () {
-    const form = document.querySelector('form');
-    const beverageForm = document.querySelector('.beverage');
-    const addBeverageButton = document.querySelector('.add-button');
-    let beverageCount = 1;
+const form = document.querySelector('form');
+const beverageForm = document.querySelector('.beverage');
+const addBeverageButton = document.querySelector('.add-button');
+let beverageCount = 1;
 
-    addBeverageButton
-        .addEventListener('click', () => {
-            beverageCount++;
-            const clonedBeverageForm = beverageForm.cloneNode(true);
-            clonedBeverageForm.querySelector('.beverage-count').textContent = `Напиток №${beverageCount}`
-            form.insertBefore(clonedBeverageForm, addBeverageButton.parentNode);
-        });
-})();
-
-(function () {
-    const modal = document.querySelector('.modal');
-    const submitButton = document.querySelector('.submit-button');
-    const modalClose = document.querySelector('.modal-close');
-
-    submitButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        modal.style.display = 'block'
+addBeverageButton
+    .addEventListener('click', () => {
+        beverageCount++;
+        const clonedBeverageForm = beverageForm.cloneNode(true);
+        clonedBeverageForm.querySelector('.beverage-count').textContent = `Напиток №${beverageCount}`
+        form.insertBefore(clonedBeverageForm, addBeverageButton.parentNode);
     });
 
-    modalClose.addEventListener('click', () => modal.style.display = 'none');
+const modal = document.querySelector('.modal');
+const submitButton = document.querySelector('.submit-button');
+const modalContent = document.querySelector('.modal-content');
+const modalClose = document.querySelector('.modal-close');
 
-    window.addEventListener('click', (e) => {
-        if (e.target === modal) {
-            modal.style.display = 'none';
-        }
-    });
-})();
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    modalContent.textContent = `Вы заказали ${formatDrinkWord(beverageCount)}`;
+    modal.style.display = 'block'
+});
+
+modalClose.addEventListener('click', () => modal.style.display = 'none');
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+function formatDrinkWord(count) {
+    if (count % 10 === 1 && count % 100 !== 11) {
+        return `${count} напиток`;
+    } else if (count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20)) {
+        return `${count} напитка`;
+    } else {
+        return `${count} напитков`;
+    }
+}
