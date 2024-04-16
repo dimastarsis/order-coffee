b52107cfe8da53ee2806ca2ab8d1ec1a7869408f
Alexander Sapegin <sapegin04@mail.ru>
2024-04-16 18:28:17 +0500

    make modal

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-(function() {
+(function () {
     const form = document.querySelector('form');
     const beverageForm = document.querySelector('.beverage');
     const addBeverageButton = document.querySelector('.add-button');
@@ -12,4 +11,23 @@
             clonedBeverageForm.querySelector('.beverage-count').textContent = `Напиток №${beverageCount}`
             form.insertBefore(clonedBeverageForm, addBeverageButton.parentNode);
         });
+})();
+
+(function () {
+    const modal = document.querySelector('.modal');
+    const submitButton = document.querySelector('.submit-button');
+    const modalClose = document.querySelector('.modal-close');
+
+    submitButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        modal.style.display = 'block'
+    });
+
+    modalClose.addEventListener('click', () => modal.style.display = 'none');
+
+    window.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
 })();
